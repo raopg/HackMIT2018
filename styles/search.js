bf396d973682ad9980f35d29a7bff66796a3b7af
anatasiavela <anastasiavela@berkeley.edu>
2018-09-15 18:43:14 -0700

    add search city

---
+++ styles/search.js
@@ -0,0 +1,7 @@
+function initAutocomplete() {
+	// Create the search box and link it to the UI element.
+    var input = document.getElementById('search-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    
+	Window.location.replace("../maps.html")
+}
